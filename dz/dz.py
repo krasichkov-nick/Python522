51729b4798a165b412cd82e17a841b360562d2bf
krasichkov-nick <krasichkov@yandex.ru>
2025-10-21 22:03:47 +0300

    jinja2

--- dz/dz.py
+++ dz/dz.py
@@ -1400,14 +1400,128 @@
 # if __name__ == '__main__':
 #     main()
 
-from parsers import Parser
+# from parsers import Parser
+#
+#
+# def main():
+#     for i in range(1, 4):
+#         pars = Parser(f"https://optimumbelt.ru/products/category/men?page={i}", 'belt.txt')
+#         pars.run()
+#
+#
+# if __name__ == '__main__':
+#     main()
 
 
-def main():
-    for i in range(1, 4):
-        pars = Parser(f"https://optimumbelt.ru/products/category/men?page={i}", 'belt.txt')
-        pars.run()
+# import time, json
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from webdriver_manager.chrome import ChromeDriverManager
+#
+#
+# def create_driver():
+#     options = Options()
+#     options.add_argument("--headless")
+#     options.add_argument("--disable-dev-shm-usage")
+#     options.add_argument("--v=99")
+#     options.add_argument("--no-sandbox")
+#     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+#     return driver
+#
+#
+# result = []
+# for i in range(0, 100 + 1):
+#     driver = create_driver()
+#     url = 'https://music.yandex.ru/genre/музыка%20всех%20жанров/artists' + '?page=' + str(i)
+#     driver.get(url)
+#     time.sleep(10)
+#     try:
+#         promo = driver.find_element_by_class_name("js-close")
+#         promo.click()
+#     except:
+#         pass
+#     time.sleep(2)
+#     artist__content = driver.find_elements_by_class_name('artist__content')
+#
+#     for artist in artist__content:
+#         artist_names = artist.find_element_by_class_name('d-artists')
+#         link_artist = artist_names.find_element_by_class_name('d-link').get_attribute("href")
+#         id_artist = link_artist.split('/')[-1]
+#         try:
+#             artist_genres = artist.find_element_by_class_name('d-genres').text
+#             try:
+#                 artist_cover = artist.find_element_by_class_name('artist-pics__pic').get_attribute("src")
+#             except:
+#                 artist_cover = None
+#         except:
+#             artist_genres = None
+#             try:
+#                 artist_cover = artist.find_element_by_class_name('artist-pics__pic').get_attribute("src")
+#             except:
+#                 artist_cover = None
+#
+#         # print(id_artist, artist_names.text, artist_genres, artist_cover)
+#         result.append({'id_artist': int(id_artist), 'artist_name': artist_names.text, 'artist_genre': artist_genres,
+#                        'artist_cover': artist_cover})
+#     time.sleep(2)
+#     print('quit')
+#     driver.quit()
+#
+# with open('result.json', 'w', encoding='utf-8') as fp:
+#     json.dump(result, fp, sort_keys=True, indent=4, ensure_ascii=False)
+
+from jinja2 import Template
 
 
-if __name__ == '__main__':
-    main()
+# html = """
+# {% macro input_func(name, placeholder='', type='text') %}
+#     <input type="{{ type }}" name="{{ name }}" placeholder="{{ placeholder }}">
+# {% endmacro %}
+#
+# <p>{{ input_func('firstname', 'Имя') }}</p>
+# <p>{{ input_func('lastname', 'Фамилия') }}</p>
+# <p>{{ input_func('address', 'Адрес') }}</p>
+# <p>{{ input_func('phone', 'Телефон', 'tel') }}</p>
+# <p>{{ input_func('email', 'Почта', 'email') }}</p>
+#
+# """
+
+html = """
+{% macro input_func(name, placeholder='', type='text') %}
+    <input type="{{ type }}" name="{{ name }}" placeholder="{{ placeholder }}">
+{% endmacro %}
+
+<p>{{ input_func('firstname', 'Имя') }}</p>
+<p>{{ input_func('lastname', 'Фамилия') }}</p>
+<p>{{ input_func('address', 'Адрес') }}</p>
+<p>{{ input_func('phone', 'Телефон', 'tel') }}</p>
+<p>{{ input_func('email', 'Почта', 'email') }}</p>
+
+"""
+
+tm = Template(html)
+msg = tm.render()
+
+print(msg)
+
+pages = [
+    {'url': 'index', 'title': 'Главная'},
+    {'url': 'news', 'title': 'Новости'},
+    {'url': 'about', 'title': 'О компании'},
+    {'url': 'shop', 'title': 'Магазин'},
+    {'url': 'contacts', 'title': 'Контакты'}
+]
+
+link = """
+
+<ul>
+{% for c in pages %}
+    <li><a href="/{{c.url}}" {% if c.title == 'Главная' %}class="active"{% endif %}>{{c.title}}</a></li>
+{% endfor %}
+</ul>
+"""
+
+tm = Template(link)
+msg = tm.render(pages=pages)
+
+print(msg)
